5461c0b3d668c017bffe9144c290ae296be61dc6
vfurmane <vfurmane@student.42.fr>
2021-01-18 09:51:50 +0100

    Define BUFSIZ if not defined

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 19:50:59 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/17 12:05:32 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/18 09:50:51 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@
 # include "precision.h"
 # include "specifier.h"
 # include "libft.h"
+
+# ifndef BUFSIZ
+#  define BUFSIZ 256
+# endif
 
 int	ft_printf(const char *str, ...);
 int	ft_parse_format(const char *str, int *str_index, char *buffer, int *i,
