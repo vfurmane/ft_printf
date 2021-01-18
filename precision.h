2eb3001c01913818093eb60dff5853efd4774f5e
vfurmane <vfurmane@student.42.fr>
2021-01-18 12:41:25 +0100

    Add stdarg header

--- precision.h
+++ precision.h
@@ -6,13 +6,14 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 15:26:20 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/16 15:37:47 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/18 11:36:49 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRECISION_H
 # define PRECISION_H
 
+# include <stdarg.h>
 # include "libft.h"
 
 int	ft_precision(const char *str, int *str_index, va_list args);
