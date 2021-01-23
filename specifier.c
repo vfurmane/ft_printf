cb8136012570f5cf27176a43b341f372ed6c2dde
vfurmane <vfurmane@student.42.fr>
2021-01-23 16:36:42 +0100

    Fix '%' specifier

--- specifier.c
+++ specifier.c
@@ -6,19 +6,27 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 15:47:02 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/23 15:55:37 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/23 16:36:17 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "specifier.h"
 
-char	*ft_specifier(const char *str, va_list args, int precision, char *res)
+char	*ft_create_string(char chr)
 {
 	char	*new_str;
 
+	if ((new_str = malloc(sizeof(*new_str))) == NULL)
+		return (NULL);
+	new_str[0] = chr;
+	return (new_str);
+}
+
+char	*ft_specifier(const char *str, va_list args, int precision, char *res)
+{
 	if (*str == 'd' || *str == 'i' || *str == 'u')
 		return (ft_print_int(args, precision, *str == 'u'));
-	else if (*str == 'c' || *str == '%')
+	else if (*str == 'c')
 		return (ft_print_char(args));
 	else if (*str == 's')
 		return (ft_print_string(args, res));
@@ -26,12 +34,9 @@ char	*ft_specifier(const char *str, va_list args, int precision, char *res)
 		return (ft_print_address(args));
 	else if (*str == 'x' || *str == 'X')
 		return (ft_print_hex(args, *str == 'x', precision));
+	else if (*str == '%')
+		return (ft_create_string('%'));
 	else
-	{
-		if ((new_str = malloc(sizeof(*new_str))) == NULL)
-			return (NULL);
-		new_str[0] = '\0';
-		return (new_str);
-	}
+		return (ft_create_string('\0'));
 	return (NULL);
 }
