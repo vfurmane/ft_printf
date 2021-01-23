12d3278b2786d0276343d9c265d0a31d5051f8e7
vfurmane <vfurmane@student.42.fr>
2021-01-23 12:42:26 +0100

    Fix precision when given negative, but width positive with 0 flag

--- precision.c
+++ precision.c
@@ -6,11 +6,20 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 15:25:40 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/22 16:00:56 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/23 12:41:26 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "precision.h"
+
+int	ft_predict_precision(int precision, char *res)
+{
+	if (precision < 0 && res[0] == '0')
+		precision = -ft_strlen(res);
+	else if (precision < 0)
+		precision = -1;
+	return (precision);
+}
 
 int	ft_precision(const char *str, int *str_index, va_list args, char *res)
 {
@@ -21,8 +30,7 @@ int	ft_precision(const char *str, int *str_index, va_list args, char *res)
 	{
 		(*str_index)++;
 		precision = ft_atoi(&str[*str_index]);
-		if (precision < 0)
-			precision = -1;
+		precision = ft_predict_precision(precision, res);
 	}
 	else if (res[0] == '0')
 		precision = -ft_strlen(res);
@@ -32,8 +40,7 @@ int	ft_precision(const char *str, int *str_index, va_list args, char *res)
 	{
 		(*str_index)++;
 		precision = va_arg(args, int);
-		if (precision < 0)
-			precision = -1;
+		precision = ft_predict_precision(precision, res);
 	}
 	else
 		while (str[*str_index] >= '0' && str[*str_index] <= '9')
